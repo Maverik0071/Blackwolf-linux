f9e626aa60d6559870b78ea30809889cdb94a836
Maverik0071 <55121581+Maverik0071@users.noreply.github.com>
2025-07-10 20:42:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,189 +1,277 @@
-#!/bin/bash
+# Arch Linux Installation Script
+# This script automates the installation of Arch Linux with ext4 filesystem
+# WARNING: This will erase data on the target disk. Use with caution.
 
-# Arch Linux Automated Installer
-# WARNING: This will erase the target disk! Use at your own risk.
+# Configuration - Modify these variables as needed
+TARGET_DISK="/dev/sda" # Change this to your target disk
+HOSTNAME="archlinux" # Set your desired hostname
+TIMEZONE="UTC" # Set your timezone (e.g., "America/New_York")
+LOCALE="en_US.UTF-8" # Set your locale
+KEYMAP="us" # Set your keyboard layout
+ROOT_PASSWORD="archlinux" # Set root password (change after installation!)
+USER_NAME="user" # Set your username
+USER_PASSWORD="user" # Set user password (change after installation!)
 
-# Configuration - customize these variables
-TARGET_DISK="/dev/sda" # Disk to install to
-HOSTNAME="archlinux" # System hostname
-USERNAME="user" # Main user to create
-TIMEZONE="America/New_York" # Timezone (see /usr/share/zoneinfo)
-KEYMAP="us" # Keyboard layout
-LANG="en_US.UTF-8" # System language
-INSTALL_DRIVE="nvme" # 'nvme' or 'sata' (for bootloader)
-ENABLE_ENCRYPTION=false # Set to true for encrypted disk
-ENCRYPTION_PASSWORD="" # Leave empty to be prompted
+# Partition layout (modify if needed)
+BOOT_PARTITION_SIZE="512M" # Size of boot partition
+SWAP_SIZE="8G" # Size of swap partition (set to 0 to disable swap)
 
-# Custom package lists
-BASE_PACKAGES=(
-base linux linux-firmware
-btrfs-progs dosfstools exfatprogs f2fs-tools e2fsprogs ntfs-3g
-man-db man-pages texinfo
-networkmanager git openssh sudo
-)
-
-DESKTOP_PACKAGES=(
-xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
-lightdm lightdm-gtk-greeter
-alacritty firefox
-noto-fonts noto-fonts-emoji
-)
-
-# -----------------------------------------------------------
-# DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING
-# -----------------------------------------------------------
-
-# Check if running as root
-if [ "$(id -u)" -ne 0 ]; then
-echo "This script must be run as root!"
-exit 1
+# Check if script is run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
 fi
 
-# Verify internet connection
-if ! ping -c 1 archlinux.org &> /dev/null; then
-echo "No internet connection detected!"
-echo "Please connect to the internet before running this script."
-exit 1
+# Verify target disk exists
+if [[ ! -e "$TARGET_DISK" ]]; then
+    echo "Target disk $TARGET_DISK not found!"
+    exit 1
 fi
 
-# Confirm installation
-echo "WARNING: This will erase ALL DATA on ${TARGET_DISK}!"
+# Confirm before proceeding
+echo "WARNING: This will erase ALL data on $TARGET_DISK!"
 read -p "Are you sure you want to continue? (y/N) " confirm
-if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
-echo "Installation cancelled."
-exit 0
+if [[ "${confirm,,}" != "y" ]]; then
+    echo "Installation aborted."
+    exit 1
 fi
 
-# Set up disk
-echo "Partitioning disk..."
-if [ "$INSTALL_DRIVE" = "nvme" ]; then
-BOOT_PARTITION="${TARGET_DISK}p1"
-ROOT_PARTITION="${TARGET_DISK}p2"
-else
-BOOT_PARTITION="${TARGET_DISK}1"
-ROOT_PARTITION="${TARGET_DISK}2"
-fi
-
-# Partition the disk
+# Set up partitions
+echo "Partitioning $TARGET_DISK..."
 parted -s "$TARGET_DISK" mklabel gpt
-parted -s "$TARGET_DISK" mkpart primary fat32 1MiB 513MiB
+parted -s "$TARGET_DISK" mkpart primary fat32 1MiB "$BOOT_PARTITION_SIZE"
 parted -s "$TARGET_DISK" set 1 boot on
-parted -s "$TARGET_DISK" mkpart primary btrfs 513MiB 100%
+
+if [[ "$SWAP_SIZE" != "0" ]]; then
+    parted -s "$TARGET_DISK" mkpart primary linux-swap "$BOOT_PARTITION_SIZE" "$((BOOT_PARTITION_SIZE + SWAP_SIZE))"
+    SWAP_PARTITION="${TARGET_DISK}2"
+    ROOT_PARTITION="${TARGET_DISK}3"
+else
+    ROOT_PARTITION="${TARGET_DISK}2"
+fi
+
+parted -s "$TARGET_DISK" mkpart primary ext4 "$(if [[ "$SWAP_SIZE" != "0" ]]; then echo "$((BOOT_PARTITION_SIZE + SWAP_SIZE))"; else echo "$BOOT_PARTITION_SIZE"; fi)" 100%
 
 # Format partitions
 echo "Formatting partitions..."
-mkfs.fat -F32 "$BOOT_PARTITION"
+mkfs.fat -F32 "${TARGET_DISK}1"
 
-if [ "$ENABLE_ENCRYPTION" = true ]; then
-if [ -z "$ENCRYPTION_PASSWORD" ]; then
-read -sp "Enter encryption password: " ENCRYPTION_PASSWORD
-echo
+if [[ "$SWAP_SIZE" != "0" ]]; then
+    mkswap "$SWAP_PARTITION"
+    swapon "$SWAP_PARTITION"
 fi
 
-echo "Setting up encryption..."
-echo -n "$ENCRYPTION_PASSWORD" | cryptsetup -q luksFormat "$ROOT_PARTITION" -
-echo -n "$ENCRYPTION_PASSWORD" | cryptsetup open "$ROOT_PARTITION" cryptroot -
-mkfs.btrfs -f /dev/mapper/cryptroot
-mount /dev/mapper/cryptroot /mnt
-else
-mkfs.btrfs -f "$ROOT_PARTITION"
+mkfs.ext4 "$ROOT_PARTITION"
+
+# Mount partitions
+echo "Mounting partitions..."
 mount "$ROOT_PARTITION" /mnt
-fi
-
-# Create subvolumes
-echo "Creating BTRFS subvolumes..."
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@snapshots
-btrfs subvolume create /mnt/@var_log
-umount /mnt
-
-# Mount the subvolumes
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@ "$ROOT_PARTITION" /mnt
-mkdir -p /mnt/{boot,home,.snapshots,var/log}
-mount "$BOOT_PARTITION" /mnt/boot
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@home "$ROOT_PARTITION" /mnt/home
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@snapshots "$ROOT_PARTITION" /mnt/.snapshots
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@var_log "$ROOT_PARTITION" /mnt/var/log
+mkdir -p /mnt/boot
+mount "${TARGET_DISK}1" /mnt/boot
 
 # Install base system
 echo "Installing base system..."
-pacstrap /mnt "${BASE_PACKAGES[@]}"
+pacstrap /mnt base base-devel linux linux-firmware
 
 # Generate fstab
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot into the new system
-echo "Configuring the new system..."
+echo "Chrooting into the new system..."
 arch-chroot /mnt /bin/bash <<EOF
 # Set timezone
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
 
-# Localization
-echo "LANG=$LANG" > /etc/locale.conf
-echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
-sed -i "s/^#\($LANG\)/\1/" /etc/locale.gen
+# Set locale
+echo "$LOCALE UTF-8" >> /etc/locale.gen
 locale-gen
+echo "LANG=$LOCALE" > /etc/locale.conf
 
-# Network configuration
+# Set keyboard layout
+echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+
+# Set hostname
 echo "$HOSTNAME" > /etc/hostname
-cat > /etc/hosts <<HOSTS
-127.0.0.1 localhost
-::1 localhost
-127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
-HOSTS
+
+# Configure hosts file
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+
+# Set root password
+echo "Setting root password..."
+echo "root:$ROOT_PASSWORD" | chpasswd
 
 # Create user
-useradd -m -G wheel -s /bin/bash "$USERNAME"
-echo "Set password for $USERNAME:"
-passwd "$USERNAME"
-echo "Set password for root:"
-passwd
+echo "Creating user $USER_NAME..."
+useradd -m -G wheel -s /bin/bash "$USER_NAME"
+echo "$USER_NAME:$USER_PASSWORD" | chpasswd
 
-# Enable sudo for wheel group
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+# Configure sudo
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
-# Install additional packages
-echo "Installing additional packages..."
-pacman -S --noconfirm "${DESKTOP_PACKAGES[@]}"
-
-# Enable services
-systemctl enable NetworkManager
-systemctl enable lightdm
-
-# Install bootloader
+# Install and configure bootloader
 echo "Installing bootloader..."
-bootctl install
+pacman -S --noconfirm grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 
-# Create bootloader config
-cat > /boot/loader/loader.conf <<LOADER
-default arch
-timeout 3
-editor no
-LOADER
+# Enable network time synchronization
+systemctl enable systemd-timesyncd.service
 
-# Create boot entry
-if [ "$ENABLE_ENCRYPTION" = true ]; then
-root_uuid=\$(blkid -s UUID -o value "$ROOT_PARTITION")
-cat > /boot/loader/entries/arch.conf <<ENTRY
-title Arch Linux
-linux /vmlinuz-linux
-initrd /initramfs-linux.img
-options cryptdevice=UUID=\$root_uuid:cryptroot root=/dev/mapper/cryptroot rw
-ENTRY
-else
-root_uuid=\$(blkid -s UUID -o value "$ROOT_PARTITION")
-cat > /boot/loader/entries/arch.conf <<ENTRY
-title Arch Linux
-linux /vmlinuz-linux
-initrd /initramfs-linux.img
-options root=UUID=\$root_uuid rw
-ENTRY
-fi
+# Install additional packages (customize as needed)
+pacman -S --noconfirm networkmanager sudo vim bash-completion
+systemctl enable NetworkManager.service
 EOF
 
-echo "Installation complete!"
-echo "You can now reboot into your new Arch Linux system."
-echo "Don't forget to remove the installation media!"
+# Clean up
+umount -R /mnt
+if [[ "$SWAP_SIZE" != "0" ]]; then
+    swapoff "$SWAP_PARTITION"
+fi
+
+echo "Installation complete! You can now reboot into your new Arch Linux system."
+echo "Don't forget to:"
+echo "1. Change the root and user passwords"
+echo "2. Configure your network if needed"
+echo "3. Install additional packages as required"# Arch Linux Installation Script
+# This script automates the installation of Arch Linux with ext4 filesystem
+# WARNING: This will erase data on the target disk. Use with caution.
+
+# Configuration - Modify these variables as needed
+TARGET_DISK="/dev/sda" # Change this to your target disk
+HOSTNAME="archlinux" # Set your desired hostname
+TIMEZONE="UTC" # Set your timezone (e.g., "America/New_York")
+LOCALE="en_US.UTF-8" # Set your locale
+KEYMAP="us" # Set your keyboard layout
+ROOT_PASSWORD="archlinux" # Set root password (change after installation!)
+USER_NAME="user" # Set your username
+USER_PASSWORD="user" # Set user password (change after installation!)
+
+# Partition layout (modify if needed)
+BOOT_PARTITION_SIZE="512M" # Size of boot partition
+SWAP_SIZE="8G" # Size of swap partition (set to 0 to disable swap)
+
+# Check if script is run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# Verify target disk exists
+if [[ ! -e "$TARGET_DISK" ]]; then
+    echo "Target disk $TARGET_DISK not found!"
+    exit 1
+fi
+
+# Confirm before proceeding
+echo "WARNING: This will erase ALL data on $TARGET_DISK!"
+read -p "Are you sure you want to continue? (y/N) " confirm
+if [[ "${confirm,,}" != "y" ]]; then
+    echo "Installation aborted."
+    exit 1
+fi
+
+# Set up partitions
+echo "Partitioning $TARGET_DISK..."
+parted -s "$TARGET_DISK" mklabel gpt
+parted -s "$TARGET_DISK" mkpart primary fat32 1MiB "$BOOT_PARTITION_SIZE"
+parted -s "$TARGET_DISK" set 1 boot on
+
+if [[ "$SWAP_SIZE" != "0" ]]; then
+    parted -s "$TARGET_DISK" mkpart primary linux-swap "$BOOT_PARTITION_SIZE" "$((BOOT_PARTITION_SIZE + SWAP_SIZE))"
+    SWAP_PARTITION="${TARGET_DISK}2"
+    ROOT_PARTITION="${TARGET_DISK}3"
+else
+    ROOT_PARTITION="${TARGET_DISK}2"
+fi
+
+parted -s "$TARGET_DISK" mkpart primary ext4 "$(if [[ "$SWAP_SIZE" != "0" ]]; then echo "$((BOOT_PARTITION_SIZE + SWAP_SIZE))"; else echo "$BOOT_PARTITION_SIZE"; fi)" 100%
+
+# Format partitions
+echo "Formatting partitions..."
+mkfs.fat -F32 "${TARGET_DISK}1"
+
+if [[ "$SWAP_SIZE" != "0" ]]; then
+    mkswap "$SWAP_PARTITION"
+    swapon "$SWAP_PARTITION"
+fi
+
+mkfs.ext4 "$ROOT_PARTITION"
+
+# Mount partitions
+echo "Mounting partitions..."
+mount "$ROOT_PARTITION" /mnt
+mkdir -p /mnt/boot
+mount "${TARGET_DISK}1" /mnt/boot
+
+# Install base system
+echo "Installing base system..."
+pacstrap /mnt base base-devel linux linux-firmware
+
+# Generate fstab
+echo "Generating fstab..."
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Chroot into the new system
+echo "Chrooting into the new system..."
+arch-chroot /mnt /bin/bash <<EOF
+# Set timezone
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc
+
+# Set locale
+echo "$LOCALE UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=$LOCALE" > /etc/locale.conf
+
+# Set keyboard layout
+echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+
+# Set hostname
+echo "$HOSTNAME" > /etc/hostname
+
+# Configure hosts file
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+
+# Set root password
+echo "Setting root password..."
+echo "root:$ROOT_PASSWORD" | chpasswd
+
+# Create user
+echo "Creating user $USER_NAME..."
+useradd -m -G wheel -s /bin/bash "$USER_NAME"
+echo "$USER_NAME:$USER_PASSWORD" | chpasswd
+
+# Configure sudo
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Install and configure bootloader
+echo "Installing bootloader..."
+pacman -S --noconfirm grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Enable network time synchronization
+systemctl enable systemd-timesyncd.service
+
+# Install additional packages (customize as needed)
+pacman -S --noconfirm networkmanager sudo vim bash-completion
+systemctl enable NetworkManager.service
+EOF
+
+# Clean up
+umount -R /mnt
+if [[ "$SWAP_SIZE" != "0" ]]; then
+    swapoff "$SWAP_PARTITION"
+fi
+
+echo "Installation complete! You can now reboot into your new Arch Linux system."
+echo "Don't forget to:"
+echo "1. Change the root and user passwords"
+echo "2. Configure your network if needed"
+echo "3. Install additional packages as required"
